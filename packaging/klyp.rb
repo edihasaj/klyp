13992b4b7e118a77e5f293bea1cb465e9313c7f7
edihasaj <edihasaj@gmail.com>
2026-05-11 22:43:05 +0200

    chore: bump cask to 0.1.7

--- packaging/klyp.rb
+++ packaging/klyp.rb
@@ -1,6 +1,6 @@
 cask "klyp" do
-  version "0.1.6"
-  sha256 "3f61d7ae35e199f534a03ef2ff47b7221992eff24334dfe14776586ac39ffced"
+  version "0.1.7"
+  sha256 "b6b4e700a1229b80013c8f16e8208c46e272d731960f57787838432308571610"
 
   url "https://github.com/edihasaj/klyp/releases/download/v#{version}/Klyp.app.zip"
   name "Klyp"
